af3d33fccab27b26a842b035c650776e67167bd1
Valentin Berlier <berlier.v@gmail.com>
2018-07-25 19:57:49 +0200

    Setup project

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+
+with open('requirements.txt') as requirements:
+    dependencies = [line.strip() for line in requirements]
+
+
+setup(
+    name='endermite',
+    version='0.0.1',
+    license='MIT',
+    description='A high-level, opinionated python framework for building Minecraft data packs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Valentin Berlier',
+    author_email='berlier.v@mail.com',
+    url='https://github.com/vberlier/endermite',
+
+    platforms=['any'],
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='minecraft datapack python framework mcfunction map-making',
+
+    packages=find_packages(),
+
+    install_requires=dependencies
+)
---
+++ tests/test_endermite.py
@@ -0,0 +1,5 @@
+import endermite
+
+
+def test_version():
+    assert endermite.__version__ == '0.0.1'
