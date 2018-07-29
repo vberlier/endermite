030a4827514b32bca2dd6a97806ba75bc06b85a5
Valentin Berlier <berlier.v@gmail.com>
2018-07-29 18:54:13 +0200

    Start implementing component building

--- endermite/component.py
+++ endermite/component.py
@@ -1,11 +1,41 @@
 __all__ = ['Component', 'ComponentBuilder']
 
 from .resource import AutoRegisteringResourceClass, ResourceBuilder
+from .function import FunctionBuilder
+from .mixins import CommandMixin
+from .decorators import FunctionData
 
 
-class Component(AutoRegisteringResourceClass):
+class Component(AutoRegisteringResourceClass, CommandMixin):
     pass
 
 
+class ComponentMethodBuilder(ResourceBuilder):
+    child_builders = (FunctionBuilder,)
+    guard_name = 'component method'
+
+    def build(self):
+        component = self.parent
+        prefix = f'{component.parent.name}:component/{component.name}/'
+
+        path = self.name
+        if self.resource.data['visibility'] == 'private':
+            path = '_private/' + path
+
+        with FunctionBuilder(self.ctx, prefix + path, []).current() as builder:
+            self.resource()
+            builder.build()
+
+
 class ComponentBuilder(ResourceBuilder):
+    child_builders = (ComponentMethodBuilder,)
     guard_name = 'component'
+
+    def build(self):
+        instance = self.resource(context=self.ctx)
+        for name in instance.defined_members:
+            member = getattr(instance, name)
+
+            if isinstance(getattr(member, 'data', None), FunctionData):
+                if 'visibility' in member.data:
+                    self.delegate(ComponentMethodBuilder, name, member)
---
+++ endermite/mixins.py
@@ -0,0 +1,19 @@
+__all__ = ['CommandMixin']
+
+from .function import FunctionBuilder
+
+
+class ContextAwareMixin:
+    def __init__(self, *args, context, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ctx = context
+
+
+class CommandMixin(ContextAwareMixin):
+    """Expose context-modifying methods that mirror minecraft commands."""
+
+    def _run(self, command_object):
+        self.ctx[FunctionBuilder].resource.append(command_object)
+
+    def say(self, *args, sep=' '):
+        self._run('say ' + sep.join(map(str, args)))  # TEMPORARY
