9ca5ca6f4e234af977f1dfde41eb7d6c9e51ab21
Valentin Berlier <berlier.v@gmail.com>
2018-09-22 21:49:07 +0200

    Add docstrings to decorators

--- endermite/decorators.py
+++ endermite/decorators.py
@@ -4,26 +4,30 @@ from .component_method import ComponentMethod
 
 
 def public(method):
-    """Set the visibility of a given method to `public`."""
+    """Make a the method public."""
     return ComponentMethod.apply(method, visibility='public')
 
 
 def private(method):
-    """Set the visibility of a given method to `private`."""
+    """Make the method private."""
     return ComponentMethod.apply(method, visibility='private')
 
 
 def tick(method):
+    """Make the method execute every tick."""
     return ComponentMethod.apply(method, tick=True)
 
 
 def load(method):
+    """Make the method execute when the datapack loads."""
     return ComponentMethod.apply(method, load=True)
 
 
 def init(method):
+    """Make the method execute when the component gets attached."""
     return ComponentMethod.apply(method, init=True)
 
 
 def destroy(method):
+    """Make the method execute when the component gets detached."""
     return ComponentMethod.apply(method, destroy=True)
