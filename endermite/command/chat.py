50963324d58039bf8da952b37a44101622ff8940
Valentin Berlier <berlier.v@gmail.com>
2018-09-23 20:30:02 +0200

    Add simple log and error methods

--- endermite/command/chat.py
+++ endermite/command/chat.py
@@ -1,6 +1,17 @@
 __all__ = ['ChatCommandMixin']
 
+import json
+
 
 class ChatCommandMixin:
     def say(self, *args, sep=' '):
         self.run('say', sep.join(map(str, args)))
+
+    def log(self, *args, sep=' ', color='gray', prefix='[LOG]'):
+        self.run('tellraw', '@a', json.dumps({
+            'text': prefix + ' ' + sep.join(map(str, args)),
+            'color': color
+        }))
+
+    def error(self, *args, sep=' '):
+        self.log(*args, sep, color='red', prefix='[ERR]')
